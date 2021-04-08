1b33098c4a39fbfe721866404e75280cc609ece5
jie.yu <jie.yu@windows10.microdone.cn>
2021-04-08 17:54:42 +0800

    20210408 commit

--- src/leetcode086/Main.java
+++ src/leetcode086/Main.java
@@ -1,9 +1,25 @@
 package leetcode086;
 
 public class Main {
+    
+    public static final void showList(ListNode head) {
+        if (head != null) {
+            System.out.println(head.val);
+            showList(head.next);
+        }
+    }
 
 	public static void main(String[] args) {
-		
+	    int[] values = {1};
+        ListNode head = new ListNode(values[0]);
+        ListNode p = head;
+        for (int i = 1; i < values.length; i++) {
+            p.next = new ListNode(values[i]);
+            p = p.next;
+        }
+        Solution s = new Solution();
+        head = s.partition(head, 0);
+        showList(head);
 	}
 
 }
--- src/leetcode086/Solution.java
+++ src/leetcode086/Solution.java
@@ -3,57 +3,36 @@ package leetcode086;
 /** <a>https://leetcode.com/problems/partition-list/</a>*/
 
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
  */
 public class Solution {
 
     public ListNode partition(ListNode head, int x) {
-        if (head == null) return null;
-        // search
-        if (head.val == x) {
-            ListNode p = head;
-            while (p.next != null) {
-                if (p.next.val < x) {
-                    ListNode q = p.next;
-                    // delete
-                    p.next = q.next;
-                    // push
-                    q.next = head;
-                    head = q;
-                    break;
-                }
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode fakeHead = new ListNode(Integer.MIN_VALUE, head);
+        ListNode insert = fakeHead;
+        while (insert.next != null) {
+            if (insert.next.val >= x) {
+                break;
+            }
+            insert = insert.next;
+        }
+        ListNode p = insert;
+        while (p.next != null) {
+            if (p.next.val < x) {
+                ListNode q = p.next;
+                p.next = p.next.next;
+                q.next = insert.next;
+                insert.next = q;
+                insert = insert.next;
+            } else {
                 p = p.next;
             }
         }
-        ListNode p = head;// pre x
-        while (p.next != null) {
-            if (p.next.val == x) {
-                break;
-            }
-            p = p.next;
-        }
-        
-        // move
-        ListNode q = p;// pre less than x
-        while (q.next != null) {
-            if (q.next.val < x) {
-                ListNode r = q.next;
-                // delete
-                q.next = r.next;
-                // insert
-                r.next = p.next;
-                p.next = r;
-                p = r;
-            } else {
-                q = q.next;
-            }
-        }
-        return head;
+        return fakeHead.next;
     }
-    
+
 }
