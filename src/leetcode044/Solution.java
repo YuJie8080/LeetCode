a5beb088307941498e0cc6702cf78dde68a39a62
jie.yu <jie.yu@windows10.microdone.cn>
2021-04-12 16:20:23 +0800

    2021年4月12日16:20:20

--- src/leetcode044/Solution.java
+++ src/leetcode044/Solution.java
@@ -1,7 +1,5 @@
 package leetcode044;
 
-// 未完成
-@Deprecated
 public class Solution {
 
     public boolean isMatch(String s, String p) {
@@ -14,18 +12,25 @@ public class Solution {
         }
         char[] sc = s.toCharArray();
         char[] pc = p.toCharArray();
-        int si = 0;
-        int pi = 0;
-        while (si < sc.length && pi < pc.length) {
-            if (pc[pi] == '*') {
-
-            } else if (pc[pi] == '?') {
-
-            } else {
-
+        boolean[][] dp = new boolean[sc.length + 1][pc.length + 1];
+        dp[0][0] = true;
+        for (int i = 0; i < pc.length; i++) {
+            if (pc[i] == '*') {
+                dp[0][i + 1] = dp[0][i];
             }
         }
-        return false;
+        for (int i = 0; i < sc.length; i++) {
+            for (int j = 0; j < pc.length; j++) {
+                if (sc[i] == pc[j] || pc[j] == '?') {
+                    dp[i + 1][j + 1] = dp[i][j];
+                } else if (pc[j] == '*') {
+                    dp[i + 1][j + 1] = dp[i][j + 1] | dp[i + 1][j];
+                } else {
+                    dp[i + 1][j + 1] = false;
+                }
+            }
+        }
+        return dp[sc.length][pc.length];
     }
 
 }
