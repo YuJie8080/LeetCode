46f25b8542fe2b018c43c971f6c4e336da66aec6
jie.yu <jie.yu@doqian.com>
2021-04-20 14:14:45 +0800

    2021年4月20日14:14:43

---
+++ src/leetcode083/Main.java
@@ -0,0 +1,25 @@
+package leetcode083;
+
+public class Main {
+
+    public static final void showList(ListNode head) {
+        if (head != null) {
+            System.out.println(head.val);
+            showList(head.next);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] values = { 1, 1, 2, 3, 3, 4, 4 };
+        ListNode head = new ListNode(values[0]);
+        ListNode p = head;
+        for (int i = 1; i < values.length; i++) {
+            p.next = new ListNode(values[i]);
+            p = p.next;
+        }
+        Solution s = new Solution();
+        head = s.deleteDuplicates(head);
+        showList(head);
+    }
+
+}
---
+++ src/leetcode083/Solution.java
@@ -0,0 +1,31 @@
+package leetcode083;
+
+public class Solution {
+    
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode fakeHead = new ListNode(Integer.MIN_VALUE, head);
+        ListNode p = fakeHead;
+        ListNode q = p.next != null ? p.next.next : null;
+        while (p != null && p.next != null && q != null) {
+            if (p.next.val == q.val) {
+                q = q.next;
+            } else {
+                if (p.next.next != q) {
+                    p.next.next = q;
+                    q = p.next != null ? p.next.next : null;
+                } else {
+                    p = p.next;
+                    q = p.next != null ? p.next.next : null;
+                }
+            }
+        }
+        if (q == null && p.next.next != q) {
+            p.next.next = q;
+        }
+        return fakeHead.next;
+    }
+
+}
